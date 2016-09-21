75541463c0c95f23d1b00a7f9e602aa066dc2803
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 16:15:06 -0500

    manage by reduxform

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,24 +1,27 @@
 import React, {Component} from 'react'
-import {reduxForm} from 'redux-form' // kinda like connect
+import {reduxForm, Field} from 'redux-form' // kinda like connect
 
 class PostsNew extends Component {
   render() {
+    const { fields: {title, categories, content}, handleSubmit} = this.props
+    // const title = this.props.fields.title
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className='form-group'>
           <label>Title</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control" {...title}/>
         </div>
 
         <div className='form-group'>
           <label>Categories</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control" {...categories}/>
         </div>
 
         <div className='form-group'>
           <label>Content</label>
-          <textarea className="form-control"/>
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
