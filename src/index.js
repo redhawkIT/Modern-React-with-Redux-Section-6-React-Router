b45e3ef4ab637a41ffca18c2d5dc93249197ce7c
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 11:40:11 -0500

    add react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { Router, browserHistory } from 'react-router'
+import reducers from './reducers'
+import routes from './routes'
 
-import App from './components/app';
-import reducers from './reducers';
+// www.blog.com/post/5
+// whenever /post/5 changes browserHistory tells react-router to update
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// hashHistory /# everything after the hash
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
