948454a7e259a7bb2d5825e28feca6e80f672e02
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 15:56:15 -0500

    setup reduxForm

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,9 +1,45 @@
 import React, {Component} from 'react'
+import {reduxForm} from 'redux-form' // kinda like connect
 
-export default class PostsNew extends Component {
+class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create A New Post</h3>
+        <div className='form-group'>
+          <label>Title</label>
+          <input type="text" className="form-control"/>
+        </div>
+
+        <div className='form-group'>
+          <label>Categories</label>
+          <input type="text" className="form-control"/>
+        </div>
+
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea className="form-control"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content'] // watch for these inputs
+})(PostsNew)
+
+// user types somthing in ... record it on application state
+
+// state === {
+//   form: {
+//     PostsNewForm: {
+//       title: '...',
+//       categories: '...',
+//       content: '...'
+//     }
+//   }
+// }
