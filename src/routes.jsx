a2f0f677e6fd69737c2a41d18c5d7bb2e45c0b59
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 14:12:20 -0500

    add posts index

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 
+
 import App from './components/app'
-
-// test component
-
-const Greeting = () => {
-  return <div>Testing 123</div>
-}
+import PostsIndex from './components/posts_index'
 
 export default(
   <Route path="/" component={App}>
-    <Route path="greet" component={Greeting}/>
-    <Route path="greet2" component={Greeting}/>
-    <Route path="greet3" component={Greeting}/>
+    <IndexRoute component={PostsIndex} />
   </Route>
 )
