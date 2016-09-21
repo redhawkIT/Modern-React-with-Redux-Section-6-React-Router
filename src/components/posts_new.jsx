864cce5ad79b1e306bb16e0d20f097882a341169
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-21 16:37:37 -0500

    add Form Validation

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -13,6 +13,9 @@ class PostsNew extends Component {
         <div className='form-group'>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
+          <div className='text-help'>
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className='form-group'>
@@ -30,12 +33,22 @@ class PostsNew extends Component {
     )
   }
 }
+
+function validate(values) {
+  const errors = {}
+  if(!values.title) {
+    errors.title = 'Enter a username'
+  }
+  return errors
+}
+
 // connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
 // reduxForm: 1st is from config, 2nd is mapStatetoProps, 3rd is mapDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content'] // watch for these inputs
+  fields: ['title', 'categories', 'content'], // watch for these inputs
+  validate
 }, null, {createPost})(PostsNew)
 
 // user types somthing in ... record it on application state
