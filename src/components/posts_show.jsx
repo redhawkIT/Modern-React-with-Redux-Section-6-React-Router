072a822a12f3fe7ed97aad03152f4899ab978341
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-09-22 10:45:52 -0500

    route after delete post

--- src/components/posts_show.jsx
+++ src/components/posts_show.jsx
@@ -1,15 +1,21 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {fetchPost, deletePost} from '../actions/index'
 import {Link} from 'react-router'
-
 class PostsShow extends Component {
+  
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id)
   }
 
   onDeleteClick() {
-    this.props.deletePost(this.props.params.id)
+    this.props.deletePost(this.props.params.id).then(() => {
+      this.context.router.push('/')
+    })
   }
 
   render() {
@@ -22,9 +28,7 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
-        <button
-          className="btn btn-danger pull-xs-right"
-          onClick={this.onDeleteClick.bind(this)}>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>
           Delete Post
         </button>
         <h3>{post.title}</h3>
